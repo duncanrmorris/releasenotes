77c2c3f61d51109d95c4b97e2b649b8d563e9854
Duncan Morris <duncanmorris07@outlook.com>
2020-10-06 09:49:02 +0200

    Update ReleaseNotesProvider.php

--- src/ReleaseNotesProvider.php
+++ src/ReleaseNotesProvider.php
@@ -11,7 +11,9 @@ class ReleaseNotesProvider extends ServiceProvider
     {
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadViewsFrom(__DIR__.'/views','releasenotes');
-        
+        $this->publishes([
+            __DIR__.'/app/Http/Controllers/ReleaseNotesController.php' => config_path('/src/Http/Controllers/ReleaseNotesController.php'),
+        ]);
     }
 
     public function register()
