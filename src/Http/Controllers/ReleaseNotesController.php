0af5850277cd4afde9a5f37f2960efb304cf8775
Duncan Morris <duncanmorris07@outlook.com>
2020-10-06 09:24:32 +0200

    Create ReleaseNotesController.php

---
+++ src/Http/Controllers/ReleaseNotesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace duncanrmorris\releasenotes\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ReleaseNotesController extends Controller
+
+{
+
+    public function index()
+    {
+        
+        return view('releasenotes::notes');
+    }
+
+}
