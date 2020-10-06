d89e369f3537a1cf65abf18aacf0e85a86a79433
Duncan Morris <duncanmorris07@outlook.com>
2020-10-06 08:31:23 +0200

    general setup and testing

--- src/ReleaseNotesProvider.php
+++ src/ReleaseNotesProvider.php
@@ -9,7 +9,7 @@ class ReleaseNotesProvider extends ServiceProvider
     
     public function boot()
     {
-
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
     }
 
     public function register()
---
+++ src/routes/web.php
@@ -0,0 +1,5 @@
+<?php
+
+Route::get('/releasenotes', function () {
+    return "hello world";
+});
