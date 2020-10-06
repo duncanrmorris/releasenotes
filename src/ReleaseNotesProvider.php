32ac13491d435f98d08ddb3d464c1b499c5d5736
Duncan Morris <duncanmorris07@outlook.com>
2020-10-06 08:22:35 +0200

    first setup and testing

---
+++ src/ReleaseNotesProvider.php
@@ -0,0 +1,20 @@
+<?php
+namespace duncanrmorris\releasenotes;
+
+use Illuminate\Support\ServiceProvider;
+
+class ReleaseNotesProvider extends ServiceProvider
+
+{
+    
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+        
+    }
+}
+
